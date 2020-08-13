777c9b48c9a53a6973a531ec5848f08272fc13fd
ilham513 <ilham513@outlook.com>
2020-08-13 07:32:39 +0700

    nambah update footer include_kaskus

--- include_kaskus.php
+++ include_kaskus.php
@@ -1,6 +1,17 @@
+<br/>.<br/>.<br/>.<br/>.
+<center>
+<h1 style="color: red;"><strong>THREAT INI KERAMEAN, huhu.</strong></h1>
+<p><strong><img src="https://i.postimg.cc/yYR35N9m/image.png" alt="" width="578" height="171" /></strong></p>
+<h1 style="color: red;"><strong>WEBSITE SAYA SAMPE JEBOL!</strong></h1>
+<p><img src="https://i.postimg.cc/fb8VXSnN/image.png" alt="" width="816" height="355" /></p>
+<h1 style="color: green;"><strong>Makasih banget Lho Buat Yang Udah Ngeshare Ke Media Sosial Kalian!</strong></h1>
+<p><strong><img src="https://i.postimg.cc/fR97cNML/image.png" alt="" width="1235" height="359" /></strong></p>
+</center>
+<br/>.<br/>.<br/>.<br/>.
+
 		
 		<div style="text-align: right;">
-		<h1 style="color:DodgerBlue;">Terima Kasih Sudah Mampir Ke Trit Sederhana Ini! Jangan Lupa Berkomentar.</h1><br/>
+		<h1 style="color:DodgerBlue;">Sekali Lagi, Terima Kasih Sudah Mampir Ke Trit Sederhana Ini! Jangan Lupa Berkomentar.</h1><br/>
 		<h2 style="color:red;"> (Karena Komentar Kalian Semangat Saya Untuk Terus Update!) </h2><br/>
 		<img id="img-tar" src="#"/><br/>.<br/>.<br/>.<br/>..<br/>.<br/>.<br/>.
 		
@@ -11,8 +22,9 @@
 		<br/>.
 		<br/>.
 		<br/>.
-		<h2>Komentar Terbaik Minggu Ini</h2>
-		<img src="http://tiny.cc/komentar-kaskus-terbaik"/>
+		<h2>2 Komentar Terbaik Minggu Ini (Update!)</h2>
+		<img src="http://tiny.cc/komentar-kaskus-terbaik"/><br/>
+		<img src="https://i.postimg.cc/KYZBSyr0/KASKUS.png"/><br/>
 		</center>
 		
 		<br/>
@@ -37,7 +49,7 @@
 
 		?>
 		
-		<h2><img src="https://bantuan.kaskus.co.id/hc/article_attachments/115007171267/smilies_fber17aocqul.gif"/> --- Baca Juga --- <img src="https://bantuan.kaskus.co.id/hc/article_attachments/115007171267/smilies_fber17aocqul.gif"/></h2>
+		<h2><img src="https://bantuan.kaskus.co.id/hc/article_attachments/115007171267/smilies_fber17aocqul.gif"/> --- Wajib Baca Trit Ini Juga --- <img src="https://bantuan.kaskus.co.id/hc/article_attachments/115007171267/smilies_fber17aocqul.gif"/></h2>
 		
 		<br/>.<br/>.<br/>.
 		<h2><a href="<?echo $ts_link[0]?>" class="kaskus"><?echo $ts_jdl[0]?></a></h2>
@@ -56,4 +68,3 @@
 		<br/>.
 
 
-		
