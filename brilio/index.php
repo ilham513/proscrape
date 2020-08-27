2fa3dd980e6d2872fa7d40b305ebdeb7cd531f09
ilham513 <ilham513@outlook.com>
2020-08-27 12:16:03 +0700

    brilio index baru

--- brilio/index.php
+++ brilio/index.php
@@ -1,3 +1,4 @@
+<? include_once('../simple_html_dom.php'); ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -13,15 +14,56 @@
 
     <!-- Custom styles for this template -->
     <link href="https://getbootstrap.com/docs/4.0/examples/sign-in/signin.css" rel="stylesheet">
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   </head>
 
-  <body class="text-center">
-    <form class="form-signin" action="go.php" method="get">
-      <img class="mb-4" src="https://cdn-brilio-net.akamaized.net/static/www/assets/v3/img/logo-top-1.png" alt="" width="132" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">URL</h1>
-      <input onfocus="this.value=''" type="text" name="url" class="form-control" placeholder="https://www.brilio.net/..." required autofocus><hr/>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">GO</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-    </form>
+  <body class="text-center">	
+	<div class="container">
+	  <div class="row">
+		<div class="col-md-4" style="height: 255px;text-align: left;overflow: auto;font-size: 24px;">
+	    <?
+			$url = "https://www.brilio.net/ngakak/";
+			$html = file_get_html($url);
+			// echo $html;die();
+
+			foreach($html->find('a') as $element) {
+				$element->href = "go.php?url=https://www.brilio.net".$element->href;
+			}
+
+			foreach($html->find('a[target=_self]') as $element) {
+				echo $element."<br/>";
+			}
+		?>
+		</div>
+		<div class="col-md-4">
+			  <form class="form-signin" action="go.php" method="get">
+			  <img class="mb-4" src="https://cdn-brilio-net.akamaized.net/static/www/assets/v3/img/logo-top-1.png" alt="" width="132" height="72">
+			  <h1 class="h3 mb-3 font-weight-normal">URL</h1>
+			  <input onfocus="this.value=''" type="text" name="url" class="form-control" placeholder="https://www.brilio.net/..." required autofocus><hr/>
+			  <button class="btn btn-lg btn-primary btn-block" type="submit">GO</button>
+			  <p class="mt-5 mb-3 text-muted">&copy; 2020 by ilham513</p>
+			  </form>
+		</div>
+		<div class="col-md-4" style="height: 255px;text-align: left;overflow: auto;font-size: 24px;">
+	    <?
+			$url = "https://www.brilio.net/selebritis/";
+			$html = file_get_html($url);
+			// echo $html;die();
+
+			foreach($html->find('a') as $element) {
+				$element->href = "go.php?url=https://www.brilio.net".$element->href;
+			}
+
+			foreach($html->find('a[target=_self]') as $element) {
+				echo $element."<br/>";
+			}
+		?>
+		</div>
+	  </div>
+	</div>
+	
+	<script>
+	</script>
   </body>
 </html>
