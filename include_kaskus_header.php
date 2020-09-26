1390b856c82889b66293936b098cda0800567db2
ilham513 <ilham513@outlook.com>
2020-09-26 10:14:22 +0700

    ROMBAK BRILIO + REVISI LINK GAMBAR KE YOUTUBE + minor revisi quotes

--- include_kaskus_header.php
+++ include_kaskus_header.php
@@ -1,5 +1,7 @@
 <center>
 .<br/>.<br/>
-<h2 style="color:red;">Tolong Bantu Tonton Video Youtube Saya!</h2>
-<img src="https://i.postimg.cc/x8N1fqYx/Angry.gif"/><br/>
+[QUOTE]
+<h2 style="color:red;">Tolong Bantu Tonton Video Youtube Saya, Pleaseee!</h2>
+<img src="http://pa1.narvii.com/5896/a49b6ae81b9822416f5ca9ecda1289da3b68585f_hq.gif"/><br/>
+[/QUOTE]
 </center>
