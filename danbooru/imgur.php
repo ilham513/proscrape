c841599996afe448dbe8549fdca4541aaa24827b
ilham513 <ilham513@outlook.com>
2021-08-17 15:29:45 +0700

    imgur {} 5 - token

--- danbooru/imgur.php
+++ danbooru/imgur.php
@@ -1,6 +1,25 @@
-<title>TES 5</title>
+<title>TES 5 - token</title>
 
 <?php
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.imgur.com/oauth2/token',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => array('refresh_token' => '{{refreshToken}}','client_id' => 'd717b9131b6e4f9','client_secret' => 'd24eb52f12aef66d483b019b106e62cc6fa121f1','grant_type' => 'refresh_token'),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
 
 $curl = curl_init();
 
@@ -15,7 +34,7 @@ curl_setopt_array($curl, array(
   CURLOPT_CUSTOMREQUEST => 'POST',
   CURLOPT_POSTFIELDS => array('image' => 'https://i.imgur.com/ExTjGWo.png'),
   CURLOPT_HTTPHEADER => array(
-    'Authorization: Bearer d24eb52f12aef66d483b019b106e62cc6fa121f1'
+    'Authorization: Client-ID d717b9131b6e4f9'
   ),
 ));
 
