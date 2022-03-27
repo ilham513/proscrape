0a9c08782d2ac439f56e121c2d15665fd952a0f3
ilham513 <ilham513@outlook.com>
2022-03-27 14:41:58 +0700

    ddesu

---
+++ ddesu/go.php
@@ -0,0 +1,19 @@
+<textarea rows="20" cols="120">
+<!-- wp:image {"id":12,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?= $_POST['gambar'] ?>" alt="" class="wp-image-12"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p><strong>Sinopsis</strong><br><?= $_POST['sinopsis'] ?>.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"width":100,"align":"center","className":"is-style-fill"} -->
+<div class="wp-block-button aligncenter has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link" href="http://ouo.io/qs/wPpb4LHb?s=<?= $_POST['link'] ?>" target="_blank" rel="noreferrer noopener">Download PDF</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:paragraph -->
+<p><h4>Binggung? <span style="text-decoration:underline;"><a href="https://komikanime18.wordpress.com/cara-download/">Baca Panduan Download</a></span></h4></p>
+<!-- /wp:paragraph -->
+</textarea>
---
+++ ddesu/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<title>INDEX</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+<div class="w3-container"><br/>
+<form class="w3-container w3-card-4" action="go.php" method="post">
+  <h2 class="w3-text-blue">Input </h2>
+  <p>      
+  <label class="w3-text-blue"><b>Gambar</b></label>
+  <input class="w3-input w3-border" name="gambar" type="text"></p>
+  <p>      
+  <label class="w3-text-blue"><b>Sinopsis</b></label>
+  <input class="w3-input w3-border" name="sinopsis" type="text"></p>
+  <p>      
+  <label class="w3-text-blue"><b>Link</b></label>
+  <input class="w3-input w3-border" name="link" type="text"></p>
+  <p>      
+  <button class="w3-btn w3-blue">GO</button></p>
+</form>
+</div>
+
+</body>
+</html> 
