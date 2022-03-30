a84605fb9264d1571f4d2042368c2ae680bc8cfa
ilham513 <ilham513@outlook.com>
2022-03-30 14:18:02 +0700

    penbamhasan 3 post

--- ddesu/go.php
+++ ddesu/go.php
@@ -1,3 +1,30 @@
+<h4><?= ucfirst($_POST['judul']) ?></h4>
+<hr/>
+<?php
+	if($_POST['tipe']=='komik'){
+		$tipe = 'komik';
+		$dot = 'PDF';
+	}elseif($_POST['tipe']=='ilustrasi'){
+		$tipe = 'ilustrasi';
+		$dot = 'PDF';
+	}else{
+		$tipe = 'shorts';
+		$dot = 'MP4';
+	}
+?>
+
+<h4><?= ucfirst($tipe)." ".ucfirst($_POST['judul'])." ".$dot ?></h4>
+
+<hr/>
+
+<?php
+	if($_POST['money']=='true'){
+		$link = 'http://ouo.io/qs/wPpb4LHb?s='.$_POST['link'];
+	}else{
+		$link = $_POST['link'];
+	}
+?>
+
 <textarea rows="20" cols="120">
 <!-- wp:image {"id":12,"sizeSlug":"large","linkDestination":"none"} -->
 <figure class="wp-block-image size-large"><img src="<?= $_POST['gambar'] ?>" alt="" class="wp-image-12"/></figure>
@@ -9,7 +36,7 @@
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button {"width":100,"align":"center","className":"is-style-fill"} -->
-<div class="wp-block-button aligncenter has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link" href="<?= $_POST['link'] ?>" target="_blank" rel="noreferrer noopener">Download PDF</a></div>
+<div class="wp-block-button aligncenter has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link" href="<?= $link ?>" target="_blank" rel="noreferrer noopener">Download <?= $dot ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons -->
 
--- ddesu/index.php
+++ ddesu/index.php
@@ -9,6 +9,18 @@
 <form class="w3-container w3-card-4" action="go.php" method="post">
   <h2 class="w3-text-blue">Input </h2>
   <p>      
+  <label class="w3-text-blue"><b>Judul</b></label>
+  <input class="w3-input w3-border" name="judul" type="text"></p>
+  <p>      
+  <label class="w3-text-blue"><b>Tipe</b></label><br/>
+	<input class="w3-radio" type="radio" name="tipe" value="komik" checked>
+	<label>Komik</label>
+	<input class="w3-radio" type="radio" name="tipe" value="ilustrasi">
+	<label>Ilustrasi</label>
+	<input class="w3-radio" type="radio" name="tipe" value="shorts">
+	<label>Shorts</label>
+  </p>
+  <p>      
   <label class="w3-text-blue"><b>Gambar</b></label>
   <input class="w3-input w3-border" name="gambar" type="text"></p>
   <p>      
@@ -17,6 +29,13 @@
   <p>      
   <label class="w3-text-blue"><b>Link</b></label>
   <input class="w3-input w3-border" name="link" type="text"></p>
+  <p>      
+  <label class="w3-text-blue"><b>Money?</b></label><br/>
+	<input class="w3-radio" type="radio" name="money" value="false" checked>
+	<label>No</label>
+	<input class="w3-radio" type="radio" name="money" value="true">
+	<label>YE$</label>
+  </p>
   <p>      
   <button class="w3-btn w3-blue">GO</button></p>
 </form>
